710e6e047d3afaebea4212eb17e7775393daadcd
BeanCjc <1175484775@qq.com>
2019-10-06 01:42:49 +0800

    update|提交代码

--- Design Pattern/CompositePattern/Program.cs
+++ Design Pattern/CompositePattern/Program.cs
@@ -20,7 +20,7 @@ namespace CompositePattern
             Folder myFolder = new SonFolder();
             myFolder.Open();
             myFolder.Add(new SonFolder());
-            myFolder.Remove(new SonFolder());
+            myFolder.Remove(new SonFolder());//透明性使用统一的类即可完成,但需要处理叶子节点中不需要的方法
             Console.WriteLine("--------------------------------------------------------");
 
             //安全式
@@ -29,7 +29,7 @@ namespace CompositePattern
 
             Safety.Folder myFolder1 = new Safety.NextFolder();
             myFolder1.Open();
-            ((Safety.SonFolder)myFolder1).Add(new Safety.NextFolder());
+            ((Safety.SonFolder)myFolder1).Add(new Safety.NextFolder());//安全模式需要处理类之间的关系,无需处理叶子节点中无关的方法,因为叶子节点中不存在无关的方法
             ((Safety.SonFolder)myFolder1).Remove(new Safety.NextFolder());
             Console.WriteLine("--------------------------------------------------------");
             Console.ReadLine();
